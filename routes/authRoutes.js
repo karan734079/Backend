031eb30ded2136f14f24ebdf57f2b97e3b69b8bd
karan kumar <126309567+karan734079@users.noreply.github.com>
2024-12-04 22:35:26 +0530

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -14,9 +14,9 @@ const cloudinary = require("cloudinary").v2;
 
 // Configure Cloudinary
 cloudinary.config({
-    cloud_name: "dk3zxkgez",
-    api_key: "654583881466299",
-    api_secret: "_KR5paMPQ8cYAcDNtFwBK1zk6x4"
+    cloud_name: "your_name",
+    api_key: "Your_api_key",
+    api_secret: "your_secret_key"
 });
 
 const uploadImageToCloudinary = async (filePath) => {
