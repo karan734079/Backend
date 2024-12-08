f87fd9bc07a8e25d994733fe6bb1295c856e274a
karan kumar <karanjangral60@gmail.com>
2024-12-08 22:13:19 +0530

    commit

--- utils/cloudnary.js
+++ utils/cloudnary.js
@@ -4,9 +4,9 @@ const fs = require("fs");
 
 // Configure Cloudinary
 cloudinary.config({
-    cloud_name: "dk3zxkgez",
-    api_key: "654583881466299",
-    api_secret: "_KR5paMPQ8cYAcDNtFwBK1zk6x4",
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_API_KEY,
+    api_secret: process.env.CLOUDNARY_API_SECRET_KEY,
 });
 
 const uploadImageToCloudinary = async (filePath, resourceType = "image") => {
