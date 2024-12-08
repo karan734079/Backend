aaf1a8d8eb84c27fb6b3e7853f1d3e047b2411d8
karan kumar <karanjangral60@gmail.com>
2024-12-08 22:56:24 +0530

    commit

--- utils/cloudnary.js
+++ utils/cloudnary.js
@@ -1,24 +1,22 @@
 const cloudinary = require("cloudinary").v2;
 const path = require("path");
 const fs = require("fs");
+require('dotenv').config(); 
 
 // Configure Cloudinary
 cloudinary.config({
-    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
-    api_key: process.env.CLOUDINARY_API_KEY,
-    api_secret: process.env.CLOUDNARY_API_SECRET_KEY,
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDNARY_API_SECRET_KEY,
 });
 
 const uploadImageToCloudinary = async (filePath, resourceType = "image") => {
-    const result = await cloudinary.uploader.upload(
-      path.normalize(filePath),
-      { folder: "profile_photos", resource_type: resourceType } // Handle both image and video uploads
-    );
-    fs.unlinkSync(filePath);
-    return result;
-  };
-  
-  module.exports = uploadImageToCloudinary;
-  
+  const result = await cloudinary.uploader.upload(
+    path.normalize(filePath),
+    { folder: "profile_photos", resource_type: resourceType } // Handle both image and video uploads
+  );
+  fs.unlinkSync(filePath);
+  return result;
+};
 
-module.exports = uploadImageToCloudinary; 
+module.exports = uploadImageToCloudinary;
